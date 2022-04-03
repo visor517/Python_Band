7823adde53ef9e9c947dc3a9c3114a94417e2361
Anna <an.malch@gmail.com>
2022-04-04 00:00:10 +0200

    Добавлена страница для создания, редактирования и удаления статьи

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from articleapp.models import Article
+
 
 def main(request):
     title = "Main"
@@ -7,4 +13,32 @@ def main(request):
 
     return render(request, "mainapp/index.html", content)
 
-# Create your views here.
+
+# Отображение содержимого из модели Article
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'mainapp/index.html'
+
+
+# Отображение содержимого
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'mainapp/article_detail.html'
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = 'mainapp/article_new.html'
+    fields = ['title', 'author', 'category', 'content', 'image']
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'mainapp/article_edit.html'
+    fields = ['title', 'category', 'content', 'image']
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = 'mainapp/article_delete.html'
+    success_url = reverse_lazy(main)
