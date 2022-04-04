da29131cb319c8dc3057ef8c46099bf86bc31c21
Anna <an.malch@gmail.com>
2022-04-04 13:46:57 +0200

    Перенос urls из mainapp в articleapp

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,15 +1,5 @@
-from django.urls import path
-
-from mainapp.views import ArticleListView, ArticleDetailView, \
-    ArticleCreateView,  ArticleUpdateView, ArticleDeleteView
-from mainapp.views import main
+from django.urls import path, include
 
 urlpatterns = [
-    path('', ArticleListView.as_view()),
-    path('index/', main),
-    path('article/new/', ArticleCreateView.as_view(), name='article_new'),
-    path('article/<uuid:pk>/', ArticleDetailView.as_view(), name='article_detail'),
-    path('article/<uuid:pk>/edit/', ArticleUpdateView.as_view(), name='article_edit'),
-    path('article/<uuid:pk>/delete/', ArticleDeleteView.as_view(), name ='article_delete'),
-
+    path('', include('articleapp.urls'))
 ]
