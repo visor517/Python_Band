cdd6bd458dfe92a90cc60801802fd66bb57b2ff1
Vladimir Nortsev <vladimr.nortsev@gmail.com>
2022-04-12 08:08:07 +0400

    Создал модель для news

--- news/models.py
+++ news/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from authapp.models import HabrUser
 
 
 class News(models.Model):
+    author = models.ForeignKey(HabrUser, on_delete=models.DO_NOTHING,
+                               verbose_name="Автор")
     title = models.CharField(verbose_name="Название", max_length=50)
     anons = models.CharField(verbose_name="Анонс", max_length=250)
     full_text = models.TextField(verbose_name='Статья')
@@ -9,3 +12,7 @@ class News(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
