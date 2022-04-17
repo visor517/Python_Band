cf06a2fad188f4ef2e614334b913dc09536938a3
mstislav <strong13strong@gmail.com>
2022-04-17 21:06:13 +0700

    Добавление комментариев 0.9.2

--- commentapp/urls.py
+++ commentapp/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import update_comment_moderation
 
 app_name = 'commentapp'
 
 urlpatterns = [
-    path(),
+    path('update_comment_moderation/<int:pk>/<slug:type>',
+         update_comment_moderation, name='update_comment_moderation'),
 ]
