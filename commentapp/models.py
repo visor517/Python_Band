2af7acd947f20ba764fa525f017f1b0a243c16a6
mstislav <strong13strong@gmail.com>
2022-04-17 23:13:05 +0700

    Добавление комментариев 0.9.5

--- commentapp/models.py
+++ commentapp/models.py
@@ -1,6 +1,27 @@
 from django.db import models
 from authapp.models import HabrUser
 from articleapp.models import Article
+from .middleware import get_current_user
+
+
+class FilterComments(models.Manager):
+    """
+    класс - Фильтр комментариев
+    """
+
+    def get_queryset(self):
+        """
+        :return:
+        """
+        user = get_current_user()
+        if not user.is_authenticated:
+            return super().get_queryset().filter(comment_moderation=True)
+        return super().get_queryset().filter(
+            models.Q(comment_moderation=False,
+                     comment_author=get_current_user()) |
+            models.Q(comment_moderation=False,
+                     comment_article__author=get_current_user()) |
+            models.Q(comment_moderation=True))
 
 
 class Comments(models.Model):
@@ -27,6 +48,7 @@ class Comments(models.Model):
     comment_create = models.DateTimeField('Дата создания', auto_now_add=True)
     comment_update = models.DateTimeField('Дата обновления', auto_now=True)
     comment_moderation = models.BooleanField('Модерация', default=False)
+    objects = FilterComments()
 
     def __str__(self):
         """
