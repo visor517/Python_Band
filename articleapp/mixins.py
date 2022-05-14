0d6ed03f177f0292aa931d45514f1a653ebbc833
HPC <mckena@mail.ru>
2022-05-14 03:05:53 +0300

    Добавлена проверка на авторство в миксин UserIsNoBlockMixin

--- articleapp/mixins.py
+++ articleapp/mixins.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.mixins import UserPassesTestMixin
-from django.shortcuts import redirect
+from django.shortcuts import redirect, get_object_or_404
+
+from articleapp.models import Article
 
 
 class UserIsNoBlockMixin(UserPassesTestMixin):
@@ -7,8 +9,10 @@ class UserIsNoBlockMixin(UserPassesTestMixin):
 
     def test_func(self):
         user = self.request.user
+        author = get_object_or_404(Article, pk=self.kwargs['pk']).author
         if user.is_authenticated:
-            return user.check_block() is False
+            if user == author:
+                return user.check_block() is False
 
     def handle_no_permission(self):
         return redirect('/')
