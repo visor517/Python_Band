e53a4ec6b0d6145a01ade63ca6c0e8206e7aa5c7
Mstislav13 <71246531+Mstislav13@users.noreply.github.com>
2022-05-16 22:01:45 +0700

    Update views.py
    Заменил contains на icontains чтобы не зависело от регистра.

--- searchapp/views.py
+++ searchapp/views.py
@@ -14,19 +14,19 @@ def search(request):
     if request.method == "POST":
         searched = request.POST['searched']
         results = Article.objects.filter(
-            Q(content__contains=searched) | Q(title__contains=searched) |
-            Q(author__username__contains=searched) |
-            Q(author__first_name__contains=searched) |
-            Q(author__last_name__contains=searched))
+            Q(content__icontains=searched) | Q(title__icontains=searched) |
+            Q(author__username__icontains=searched) |
+            Q(author__first_name__icontains=searched) |
+            Q(author__last_name__icontains=searched))
         results_2 = Comments.objects.filter(
-            Q(comment_author__username__contains=searched) |
-            Q(comment_author__first_name__contains=searched) |
-            Q(comment_author__last_name__contains=searched) |
-            Q(comment_text__contains=searched))
+            Q(comment_author__username__icontains=searched) |
+            Q(comment_author__first_name__icontains=searched) |
+            Q(comment_author__last_name__icontains=searched) |
+            Q(comment_text__icontains=searched))
         results_3 = HabrUser.objects.filter(
-            Q(username__contains=searched) |
-            Q(first_name__contains=searched) |
-            Q(last_name__contains=searched))
+            Q(username__icontains=searched) |
+            Q(first_name__icontains=searched) |
+            Q(last_name__icontains=searched))
         return render(request, 'search.html',
                       {'searched': searched,
                        'results': results,
